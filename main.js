b4f12ccf730db1b91b2111909b3171835dcb470d
YaroslavBerkuta <yaroslavberkuta@gmail.com>
2021-12-17 18:10:34 +0300

    desktop

--- main.js
+++ main.js
@@ -36,15 +36,15 @@ const renderCalendar = () => {
   ).getDay();
   const nextDays = 7 - lastDayIndex - 1;
   const months = [
-    "январь",
+    "Январь",
     "Февраль",
-    "март",
-    "апрель",
+    "Март",
+    "Апрель",
     "Май",
-    "июнь",
-    "июль",
-    "август",
-    "сентябрь",
+    "Июнь",
+    "Июль",
+    "Август",
+    "Сентябрь",
     "Октябрь",
     "Ноябрь",
     "Декабрь",
@@ -59,7 +59,7 @@ const renderCalendar = () => {
       i === new Date().getDate() &&
       date.getMonth() === new Date().getMonth()
     ) {
-      days += `<div class="today"><span>${i}</span></div>`;
+      days += `<div class="today active"><span>${i}</span></div>`;
     } else {
       days += `<div><span>${i}</span></div>`;
     }
@@ -68,26 +68,22 @@ const renderCalendar = () => {
     days += `<div class="next-date"><span>${j}</span></div>`;
     monthDays.innerHTML = days;
   }
-  const daysDiv = document.querySelectorAll(".days div")
-
-daysDiv.forEach(item=>{
-  item.classList.remove("active");
-  item.addEventListener("click",()=>{
-    if(!item.classList.contains("active")){
-      item.classList.add("active");
-    }
-  })
-})
-
-
-for(let i=0; i<daysDiv.length; i++){
-  daysDiv[i].addEventListener("click", ()=>{
-    daysDiv.forEach(item=>{
-      item.classList.remove("active");
-    })
-    daysDiv[i].classList.add("active")
-  })
-}
+  const dateInput = document.getElementById("dateSelect");
+  const daysDiv = document.querySelectorAll(".days div");
+  for (let i = 0; i < daysDiv.length; i++) {
+    daysDiv[i].addEventListener("click", () => {
+      dateInput.setAttribute(
+        "value",
+        `${daysDiv[i].querySelector("div span").textContent}.${
+          date.getMonth() + 1
+        }.${date.getFullYear()}`
+      );
+      daysDiv.forEach((item) => {
+        item.classList.remove("active");
+      });
+      daysDiv[i].classList.add("active");
+    });
+  }
 };
 document.querySelector(".prev-btn").addEventListener("click", () => {
   date.setMonth(date.getMonth() - 1);
@@ -98,3 +94,79 @@ document.querySelector(".next-btn").addEventListener("click", () => {
   renderCalendar();
 });
 renderCalendar();
+
+const timeDiv = document.querySelectorAll(".time___flex div");
+const timeInput = document.getElementById("timeSelect");
+
+timeDiv.forEach((item) => {
+  item.classList.remove("active");
+  item.addEventListener("click", () => {
+    timeInput.setAttribute("value", item.querySelector("span").textContent);
+    if (!item.classList.contains("active")) {
+      item.classList.add("active");
+    }
+  });
+});
+
+for (let i = 0; i < timeDiv.length; i++) {
+  timeDiv[i].addEventListener("click", () => {
+    timeDiv.forEach((item) => {
+      item.classList.remove("active");
+    });
+    timeDiv[i].classList.add("active");
+  });
+}
+
+const cheack = document.querySelector(".cheack");
+const cheackImg = document.querySelector(".cheack__img");
+const behindWheels = document.querySelector(".select__wheels-behind");
+cheack.addEventListener("click", () => {
+  cheackImg.classList.toggle("active");
+  if (cheackImg.classList.contains("active")) {
+    behindWheels.classList.add("active");
+  } else {
+    behindWheels.classList.remove("active");
+  }
+});
+
+let map;
+
+
+
+function initMap() {
+  const myLatLng = { lat: 49.44298119994354, lng: 26.988555019877083 };
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 15,
+    mapId: "f6a4b25e969da642",
+    center: myLatLng,
+  });
+
+  new google.maps.Marker({
+    icon:"./img/mapPin.svg",
+    position: myLatLng,
+    map,
+    title: "German Wheels",
+  });
+}
+
+
+const firstPopupOpen = document.getElementById("firstPopup")
+const secondPopupOpen = document.getElementById("secondPopup")
+const popup = document.querySelectorAll(".popup")
+const firstPopup = document.querySelector(".popup__first")
+const secondPopup = document.querySelector(".popup__second")
+const closepopup = document.querySelectorAll(".popup__close")
+firstPopupOpen.addEventListener("click", ()=>{
+  firstPopup.classList.add("active")
+})
+secondPopupOpen.addEventListener("click", ()=>{
+  secondPopup.classList.add("active")
+})
+closepopup.forEach((item) => {
+  item.addEventListener("click", () => {
+    popup.forEach((popup)=>{
+      popup.classList.remove("active");
+    })
+  })
+})
+
